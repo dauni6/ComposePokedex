6c5ad4aaab2bc9aa59f294f07a69271b4d51a69a
dauni6 <45280927+dauni6@users.noreply.github.com>
2022-02-05 23:22:31 +0900

    3. PokemonListScreen 구현하기
    1. PokemonListScreen.kt 파일을 만들고 @Composable annotation을 이용하여
    화면 만들기
    
    2. SearchBar를 따로 @Composable 함수로 만들어서 사용하고 파라미터로 onSearch: (String) -> Unit = {} 람다를 사용.
    커뮤니티에서 데이터를 보내고 그에 대한 응답을 받기 위해 람다를 사용하라는 글들을 많이 봤는데 아마 recomposition 때문인 것 같다.

--- app/src/main/java/com/dontsu/composepokedex/MainActivity.kt
+++ app/src/main/java/com/dontsu/composepokedex/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.dontsu.composepokedex.presentation.PokemonListScreen
 import com.dontsu.composepokedex.ui.theme.ComposePokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     // 왜 composable로 만드는걸까??
                     // 일단 여기서 각각의 destination을 만드는거란건 확실하다
                     composable("pokemon_list_screen") {
-
+                        PokemonListScreen(navController = navController)
                     }
 
                     composable(
---
+++ app/src/main/java/com/dontsu/composepokedex/presentation/PokemonListScreent.kt
@@ -0,0 +1,96 @@
+package com.dontsu.composepokedex.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.dontsu.composepokedex.R
+
+@Composable
+fun PokemonListScreen(
+    navController: NavController,
+) {
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(20.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_international_pok_mon_logo),
+                contentDescription = "pokemon",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+            SearchBar(
+                hint = "Search...",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hint.isNotEmpty())
+    }
+
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                onSearch(it)
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    // TextField를 눌렀을 때 hint를 보여줄지 말지
+                    isHintDisplayed = it.isFocused.not()
+                }
+        )
+        if (isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
+
+}
